b085fe65e9247b8bc4bf08e65250c1902eba815b
Crypt0bvious <cryptobvious25@gmail.com>
2025-03-22 01:54:35 +0000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        glitch: ["'Press Start 2P'", "monospace"]
+      }
+    },
+  },
+  plugins: [],
+}
